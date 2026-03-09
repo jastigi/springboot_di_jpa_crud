242879bfcfbb96d8c1844ab7a5b6afe1aa0c4985
jastigi <jastigi@gmail.com>
2026-03-09 14:21:03 +0100

    Roles y claims

--- src/main/java/com/jastigi/curso/springboot/app/springboot_crud/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/jastigi/curso/springboot/app/springboot_crud/security/filter/JwtAuthenticationFilter.java
@@ -1,15 +1,16 @@
 package com.jastigi.curso.springboot.app.springboot_crud.security.filter;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-
-import javax.crypto.SecretKey;
 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.fasterxml.jackson.core.exc.StreamReadException;
@@ -17,17 +18,18 @@ import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jastigi.curso.springboot.app.springboot_crud.entities.User;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import static com.jastigi.curso.springboot.app.springboot_crud.security.TokenJwtConfig.*;
+
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     private AuthenticationManager authenticationManager;
-
-    private static final SecretKey SECRET_KEY = Jwts.SIG.HS256.key().build();
 
     public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
         this.authenticationManager = authenticationManager;
@@ -66,9 +68,22 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
         User user = (User) authResult.getPrincipal();
         String username = user.getUsername();
-        String token = Jwts.builder().subject(username).signWith(SECRET_KEY).compact();
+        Collection<? extends GrantedAuthority> roles = authResult.getAuthorities();
 
-        response.addHeader("Authorization", "Bearer " + token);
+        Claims claims = Jwts.claims()
+                .subject(username)
+                .build();
+
+        claims.put("authorities", roles);
+
+        String token = Jwts.builder().subject(username)
+                .claims(claims)
+                .expiration(new Date(System.currentTimeMillis() + 3600000))
+                .issuedAt(new Date())
+                .signWith(SECRET_KEY)
+                .compact();
+
+        response.addHeader(HEADER_AUTHORIZATION, PREFIX_TOKEN + token);
 
         Map<String, Object> body = new HashMap<>();
         body.put("token", token);
