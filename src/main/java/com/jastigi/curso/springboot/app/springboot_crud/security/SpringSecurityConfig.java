6397bc9a2be76e2733d4d8bd5799909f7616164c
jastigi <jastigi@gmail.com>
2026-03-12 11:22:31 +0100

    Reglas de seguridad

--- src/main/java/com/jastigi/curso/springboot/app/springboot_crud/security/SpringSecurityConfig.java
+++ src/main/java/com/jastigi/curso/springboot/app/springboot_crud/security/SpringSecurityConfig.java
@@ -37,6 +37,12 @@ public class SpringSecurityConfig {
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers(HttpMethod.GET, "/api/users").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/users/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/users").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/api/products", "/api/products/{id}")
+                        .hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.POST, "/api/products").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/api/products/{id}").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/products/{id}").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .addFilter(new JwtAuthenticationFilter(authenticationManager()))
                 .addFilter(new JwtValidationFilter(authenticationManager()))
--- src/main/java/com/jastigi/curso/springboot/app/springboot_crud/validation/IsExistDbValidation.java
+++ src/main/java/com/jastigi/curso/springboot/app/springboot_crud/validation/IsExistDbValidation.java
@@ -1,12 +1,14 @@
 package com.jastigi.curso.springboot.app.springboot_crud.validation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.jastigi.curso.springboot.app.springboot_crud.services.ProductService;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
+@Component
 public class IsExistDbValidation implements ConstraintValidator<IsExistDb, String> {
 
     @Autowired
